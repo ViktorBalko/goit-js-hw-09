3e24a231dce4d9b341f6bddb90dc475771eb7233
ViktorBalko <viktorbalko.samsung@gmail.com>
2022-11-30 00:20:24 +0200

    29.11
    #3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formElem = document.querySelector('.form');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  const elems = evt.currentTarget.elements;
+
+  let delay = Number(elems.delay.value);
+  let step = Number(elems.step.value);
+  let amount = Number(elems.amount.value);
+
+  for (let position = 1; position <= amount; position++) {
+    if (position !== 1) {
+      delay += step;
+    }
+    createPromise(position, delay)
+      .then(({ position, delay }) =>
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`),
+      )
+      .catch(({ position, delay }) =>
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`),
+      );
   }
 }
+
+formElem.addEventListener('submit', onFormSubmit);
