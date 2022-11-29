a49cdb151e4dd34f7518cc333d241f3533573a5d
ViktorBalko <viktorbalko.samsung@gmail.com>
2022-11-29 23:16:28 +0200

    29.11
    #2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startButton = document.querySelector('button[data-start]'); 
+const stopButton = document.querySelector('button[data-stop]');
+const elemBody = document.querySelector('body');
 
+stopButton.disabled = true;
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function startChangeColor() {
+  startButton.disabled = true;
+  stopButton.disabled = false;
+  timerId = setInterval(() => {
+    elemBody.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+}
+
+function stopChangeColor() {
+  clearInterval(timerId);
+  startButton.disabled = false;
+  stopButton.disabled = true;
+}
+
+startButton.addEventListener('click', startChangeColor);
+stopButton.addEventListener('click', stopChangeColor);
